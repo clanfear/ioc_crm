711432d4fa15e55c80a75d8f75c2067137a28819
Charles Lanfear <cclanfear@gmail.com>
2022-10-05 16:44:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Introduction to Quantitative Analysis
+
+[Visit the course site here](https://clanfear.github.io/ioc_iqa/_site/index.html)
+ 
---
+++ _lectures/render_lecture.R
@@ -0,0 +1,12 @@
+render_lecture <- function(x, purl = TRUE){
+  input_paths <-  sort(list.files("./_lectures/", 
+                                  pattern = "^slides.*Rmd$", 
+                                  recursive = TRUE, 
+                                  full.names = TRUE))[x+1]
+  r_out_paths <-  stringr::str_remove(input_paths, "md$")
+  unlink(r_out_paths, recursive = FALSE)
+  purrr::walk2(.x = input_paths, .y = r_out_paths, 
+               ~ knitr::purl(.x, output = .y, documentation = 0))
+  purrr::walk(.x = input_paths, ~rmarkdown::render(.x, encoding = "UTF-8"))
+}
+render_lecture(1)
