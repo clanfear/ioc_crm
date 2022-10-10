1360c8e5e9e679102b1c048157190cf279a97843
Charles Lanfear <cclanfear@gmail.com>
2022-10-10 11:25:28 +0100

    Relabel posts to forthcoming

--- _lectures/render_lecture.R
+++ _lectures/render_lecture.R
@@ -1,12 +1,14 @@
-render_lecture <- function(x, purl = TRUE){
+render_lecture <- function(x, purl = FALSE){
   input_paths <-  sort(list.files("./_lectures/", 
                                   pattern = "^slides.*Rmd$", 
                                   recursive = TRUE, 
                                   full.names = TRUE))[x]
   r_out_paths <-  stringr::str_remove(input_paths, "md$")
   unlink(r_out_paths, recursive = FALSE)
-  purrr::walk2(.x = input_paths, .y = r_out_paths, 
-               ~ knitr::purl(.x, output = .y, documentation = 0))
+  if(purl){
+    purrr::walk2(.x = input_paths, .y = r_out_paths, 
+                 ~ knitr::purl(.x, output = .y, documentation = 0))
+  }
   purrr::walk(.x = input_paths, ~rmarkdown::render(.x, encoding = "UTF-8"))
 }
 render_lecture(1)
